fba81793743cfc8109707ebbb8ab5e725c8210c9
eileenchoi <eileen.h.choi@gmail.com>
2020-04-30 16:08:31 +0100

    desktop complete

---
+++ footer.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Upply
+ */
+
+?>
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+			<img src="<?php the_field('logo') ?>" alt="Upply.io logo">
+			<p>&copy; 2020 - <?php echo date('Y'); ?> Upply.io | All Rights Reserved</p>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ front-page.php
@@ -0,0 +1,29 @@
+<?php
+
+get_header();
+?>
+
+<main id="primary" class="site-main">
+    <div class="hero-wrapper">
+        <div class="content hero-container">
+            <div class="text">
+                <h1><?php the_field('heading'); ?></h1>
+                <p><?php the_field('content'); ?></p>
+            </div>
+            <div class="contact">
+                <?php the_field('shortcode'); ?>
+            </div>
+            <p class="disclaimer">**By submitting your name and email address, you are granting us permission to contact you by email.</p>
+            <div class="icons">
+                <img src="<?php the_field('icons') ?>" alt="">
+            </div>
+        </div>
+        <div class="content image-container">
+            <img src="<?php the_field('image'); ?>" alt="">
+        </div>
+    </div>
+
+</main><!-- #main -->
+
+<?php
+get_footer();
